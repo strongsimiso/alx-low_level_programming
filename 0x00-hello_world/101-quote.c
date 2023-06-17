1f38e8dac669af83a746d15e8cd158359023725f
strongsimiso <mohamedsorf0@gmail.com>
2023-06-16 17:48:30 -0700

    T101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,10 @@
 #include <unistd.h>
 /**
 * main - Entry point
-*
 * Return: Always 1 (Success)
 */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
